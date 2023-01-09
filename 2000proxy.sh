b81f138ae44362fee04184dff92e72733ab2b025
doananh95 <121426384+doananh95@users.noreply.github.com>
2023-01-09 10:57:31 +0700

    Update 2000proxy.sh

--- 2000proxy.sh
+++ 2000proxy.sh
@@ -77,7 +77,7 @@ upload_proxy() {
     zip --password $PASS proxy.zip proxy.txt
     URL=$(curl -F "file=@proxy.zip" https://file.io)
 
-    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+    echo "Proxy is ready! Format IP:PORT"
     echo "Download zip archive from: ${URL}"
     echo "Password: ${PASS}"
 
